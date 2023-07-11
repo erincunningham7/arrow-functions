6985f9906ddd06968250d4413bc490f462e5b595
erincunningham7 <101681089+erincunningham7@users.noreply.github.com>
2023-07-11 10:48:51 +0000

    Create single line arrow func

--- arrow-functions.js
+++ arrow-functions.js
@@ -13,15 +13,19 @@
 // console.log(sum)
 
 // Arrow Function With Parameters
-const addTwoNumbers = (a, b) => {
-    // Code block
-    return a + b;
-}
+// const addTwoNumbers = (a, b) => {
+//     // Code block
+//     return a + b;
+// }
 
-let sum = addTwoNumbers(3, 5);
-console.log(sum)
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum)
 
 // Single Line Arrow Function With Parameters
+//const addTwoNumbers = (a, b) => (a + b); also valid
+const addTwoNumbers = (a, b) => a + b;
+let sum = addTwoNumbers(6, 4);
+console.log(sum)
 
 // Implicit Returns
 
